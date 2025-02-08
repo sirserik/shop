c67f38a2a621a95aaa7b8804c1620c699b73d27e
serik <m.serik@mail.ru>
2025-02-08 10:24:34 +0500

    added htaccess and start Router.php

--- core/Router.php
+++ core/Router.php
@@ -4,8 +4,49 @@ namespace Core;
 
 class Router
 {
- public function __construct()
- {
-     echo 'router';
- }
+    private $routes = [];
+    private $notFoundCallBack;
+    private $baseUrl;
+
+    public function __construct($baseUrl = '')
+    {
+        $this->baseUrl = rtrim($baseUrl,'/');
+    }
+
+
+    /**
+     * @param string $method
+     * @param string $pattern
+     * @param callable $callback
+     * @param array $options
+     * @return void
+     */
+    public function  addRoute(string $method, string $pattern, callable $callback, array $options =[]): void
+    {
+        $pattern = $this->baseUrl. rtrim($pattern,'/');
+        $pattern = preg_replace('#/{([^\/]+)}#','/(?P<$1>[^/]+)',$pattern);
+        $pattern = '#'.$pattern.'#';
+
+        $this->routes[] = [
+            'method' =>$method,
+            'pattern' => $pattern,
+            'callback'=>$callback,
+            'options' =>$options
+        ];
+    }
+
+    public function get(string $pattern,callable $callback,array $options =[]): void
+    {
+        $this->addRoute('GET',$pattern,$callback,$options);
+    }
+
+    public function post(string $pattern,callable $callback,array $options =[]): void
+    {
+        $this->addRoute('POST',$pattern,$callback,$options);
+    }
+
+    public function setNotFoundHandler(callable $callback): void
+    {
+        $this->notFoundCallBack = $callback;
+    }
 }
